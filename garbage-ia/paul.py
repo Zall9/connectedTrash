edfffec037021e0f1dd19bd35b526170b85b5399
Mathis Tanghe <77977328+blurp748@users.noreply.github.com>
2024-03-11 09:43:06 +0100

    Add files via upload

---
+++ garbage-ia/paul.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as IO
+import sys
+import time
+from numpy import interp
+
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+
+class GroveServo:
+    MIN_DEGREE = 0
+    MAX_DEGREE = 180
+    INIT_DUTY = 2.5
+
+    def __init__(self, channel):
+        IO.setup(channel,IO.OUT)
+        self.pwm = IO.PWM(channel,50)
+        self.pwm.start(GroveServo.INIT_DUTY)
+
+    def __del__(self):
+        self.pwm.stop()
+
+    def setAngle(self, angle):
+        # Map angle from range 0 ~ 180 to range 25 ~ 125
+        angle = max(min(angle, GroveServo.MAX_DEGREE), GroveServo.MIN_DEGREE)
+        tmp = interp(angle, [0, 180], [25, 125])
+        self.pwm.ChangeDutyCycle(round(tmp/10.0, 1))
+
+Grove = GroveServo
+
+def main():
+    servo = GroveServo(12)
+    while True:
+        servo.setAngle(60)
+        time.sleep(2)
+        servo.setAngle(0)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
